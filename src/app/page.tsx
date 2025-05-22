3b89ee3e6d8830eccf94035900355a0a94a5b79b
MT 澤野井 穂香 <honoka.sawanoi@metateam.co.jp>
2025-05-22 12:25:00 +0900

    レイアウト構築演習

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-const name: string = "Yuki";
-
-export default function Home() {
+export default function Page() {
+	const today = new Date().toLocaleDateString("ja-JP", {
+		year: "numeric", //年月日形式で表示する
+		month: "short",
+		day: "numeric",
+	});
+	const message = "今日も頑張りましょう！";
 	return (
-		<main>
-			<h1>こんにちは、{name}さん</h1>
-		</main>
+		<>
+			<h1>こんにちは！</h1>
+			<p>今日は {today} です。</p>
+			<p>{message}</p>
+		</>
 	);
 }
