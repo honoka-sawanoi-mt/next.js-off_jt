383d4324c1f6b2d4f04cd3c549a6c79bed3cd45a
MT 澤野井 穂香 <honoka.sawanoi@metateam.co.jp>
2025-05-28 10:19:19 +0900

    レイアウト構築演習の修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,31 +2,31 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "Today's",
+  title: "Hello",
 };
 //Readonlyはchildrenプロパティが受け取ることのできる型を示し、オブジェクトのプロパティが変更されないことを保証している
 /*React.ReactNodeはchildrenが受け取ることのできる型を示している
 Reactがレンダリング可能な全てのものを含む*/
 export default function RootLayout({
-	children,
-	modal,
+  children,
+  modal,
 }: {
-	children: React.ReactNode;
-	modal: React.ReactNode;
+  children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
-	return (
-		<html lang="ja">
-			<body>
-				<header style={{ padding: "1rem", background: "#eee" }}>
-					<h1 className="heading">Top Page</h1>
-				</header>
+  return (
+    <html lang="ja">
+      <body>
+        <header className="header">
+          <h1 className="heading">Top Page</h1>
+        </header>
 
-				<main>{children}</main>
+        <main>{children}</main>
 
-				<footer style={{ padding: "1rem", background: "#eee" }}>
-					<p>© 2024 Today's App</p>
-				</footer>
-			</body>
-		</html>
-	);
+        <footer className="footer">
+          <p>© 2024 Hello App</p>
+        </footer>
+      </body>
+    </html>
+  );
 }
