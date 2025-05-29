b2596cdcb36429177b03e30dd41c2e751e6c82de
MT 澤野井 穂香 <honoka.sawanoi@metateam.co.jp>
2025-05-29 18:51:30 +0900

    ReactHookForm入門課題の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { InputForm } from "./InputForm";
 
-export default function Form() {
+export default function Page() {
   return (
     <main>
       <InputForm />
